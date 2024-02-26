5697b6220f1aa5c7c0d22948ba9791455288d43f
Rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-02-26 17:52:16 +0000

    yes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+
+
 
 function App() {
   return (
-    <React.Fragment>
-
-    </React.Fragment>
-  
+    <>
+    {/* This is to define or tell us all the routes we have on our website */}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {/* If someone clicks on the products, it will show the Products Page */}
+      {/* <Route path="/products" element={<ProductsPage />} /> */}
+      {/* <Route path="/login" element={<LoginPage />} /> */}
+      <Route path="/register" element={<RegisterPage />} />
+      {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+    </Routes>
+  </>
   );
 }
 
